fd6228b8531cbd1683d024c464e7698848d6a5a8
lombardofer <146959738+lombardofer@users.noreply.github.com>
2024-02-07 20:46:58 -0300

    Commit ProyectoFinal

--- index.js
+++ index.js
@@ -1,62 +1,116 @@
-const productos = [
-  { id: 1, nombre: 'Apple Macbook Air M2 480GB', precio: 2000000, imagen: '../img/producto1.png' },
-  { id: 2, nombre: 'Apple Iphone 15 128GB', precio: 1300000, imagen: '../img/producto3.png' },
-  { id: 3, nombre: 'Apple Iphone 15 PRO 256GB', precio: 1250000, imagen: '../img/producto2.png' },
-  { id: 4, nombre: 'Apple Ipad M2 Air 512GB', precio: 900000, imagen: '../img/producto4.png' },
-  { id: 5, nombre: 'Apple Ipad Air Pro 1TB', precio: 1120000, imagen: '../img/producto5.png' },
-  { id: 6, nombre: 'Apple Watch Series 7', precio: 509000, imagen: '../img/producto6.png' },
-  { id: 7, nombre: 'Samsung Galaxy Book Pro 360°', precio: 1200000, imagen: '../img/producto7.png' },
-  { id: 8, nombre: 'Samsung Galaxy Book Pro', precio: 1125000, imagen: '../img/producto8.png' },
-  { id: 9, nombre: 'Samsung Galaxy Book 3', precio: 900000, imagen: '../img/producto10.png' },
-  { id: 10, nombre: 'Samsung Galaxy S23 ULTRA', precio: 999999, imagen: '../img/producto9.png' },
-  { id: 11, nombre: 'Google Home Pro', precio: 200000, imagen: '../img/producto11.png' },
-  { id: 12, nombre: 'Alexa Echo Dot', precio: 130000, imagen: '../img/producto13.png' },
-  { id: 13, nombre: 'JBL GO 3 SPEAKER', precio: 50000, imagen: '../img/producto12.png' },
-  { id: 14, nombre: 'Apple Airpods 2dn Gen', precio: 900000, imagen: '../img/producto14.png' },
-
-];
-
 document.addEventListener('DOMContentLoaded', () => {
-  const listaProductos = document.getElementById('lista-productos');
-  productos.forEach(producto => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-      <div>
-        <img src="${producto.imagen}" alt="${producto.nombre}" widht="100px" height="100px">
-        <h6>${producto.nombre}</h3>
-        <p>$${producto.precio}</p>
-        <button onclick="agregarAlCarrito(${producto.id})">Añadir al carrito</button>
-      </div>`;
-    listaProductos.appendChild(li);
-  });
-
-cargarCarrito();
-
+  cargarProductos();
+  cargarCarrito();
 });
 
 
-  function agregarAlCarrito(idProducto) {
-    let carrito = obtenerCarrito();
-    const producto = productos.find(p => p.id === idProducto);
-  
-    if (producto) {
+document.getElementById('lista-productos').addEventListener('click', event => {
+  if (event.target.matches('button.agregar-al-carrito')) {
+    const idProducto = parseInt(event.target.dataset.id);
+    agregarAlCarrito(idProducto);
+  }
+});
+
+function cargarProductos() {
+  fetch("../js/lista.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No se pudo obtener la lista de productos');
+      }
+      return response.json();
+    })
+    .then(data => {
+
+      localStorage.setItem('productos', JSON.stringify(data));
+
+      const listaProductos = document.getElementById('lista-productos');
+      data.forEach(producto => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <div>
+                <img src="${producto.imagen}" alt="${producto.nombre}" width="100px" height="100px">
+                <h6>${producto.nombre}</h6>
+                <p>$${producto.precio}</p>
+                <button class="agregar-al-carrito" data-id="${producto.id}">Añadir al carrito</button>
+            </div>`;
+        listaProductos.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error('Error al cargar los productos:', error);
+    });
+}
+
+function agregarAlCarrito(idProducto) {
+  fetch("../js/lista.json")
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('No se pudo agregar el producto al carrito');
+      }
+      return response.json();
+  })
+  .then(productos => {
+
+      const producto = productos.find(producto => producto.id === idProducto);
+      
+      if (!producto) {
+          throw new Error('Producto no encontrado');
+      }
+
+      Toastify({
+          text: "Acabas de agregar el producto al carrito con éxito",
+          className: "info",
+          style: {
+              background: "linear-gradient(to right, red, yellow)",
+          }
+      }).showToast();
+
+      let carrito = obtenerCarrito();
       carrito.push(producto);
       guardarCarrito(carrito);
       actualizarCarritoDOM();
-    }
-  }
+  })
+  .catch(error => {
+      console.error('Error al agregar el producto al carrito:', error);
+  });
+}
 
-  function verCarrito() {
-    const carritoDiv = document.getElementById('carrito');
-    carritoDiv.style.display = carritoDiv.style.display === 'none' ? 'block' : 'none';
-  }
+function cargarCarrito() {
+  fetch("../lista.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No se pudo cargar el carrito');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const listaCarrito = document.getElementById('lista-carrito');
+      listaCarrito.innerHTML = '';
+      data.forEach(producto => {
+        const li = document.createElement('li');
+        li.textContent = `${producto.nombre} - $${producto.precio}`;
+        listaCarrito.appendChild(li);
+      });
+    })
+    .catch(error => {
+      console.error('Error al cargar el carrito:', error);
+    });
+}
+
 
   //  LOCAL STORAGE - FUNCIONES PARA REMOVER CARRITO, CARGARLO Y GUARDARLO PARA EL SIGUIENTE INICIO" //
-
 
   function vaciarCarrito() {
     localStorage.removeItem('carrito');
     actualizarCarritoDOM();
+    Toastify({
+      text: "Acabas de vaciar tu carrito con éxito",
+      className: "info",
+      style: {
+        background: "linear-gradient(to right, red, yellow)",
+      }
+    }).showToast();
+    
   }
   
   function cargarCarrito() {
@@ -87,6 +141,3 @@ cargarCarrito();
       listaCarrito.appendChild(li);
     });
   }
-
-  
-  
